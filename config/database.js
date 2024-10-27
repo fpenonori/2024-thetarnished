672220d36dcd2ef3f6a45cc65d9e6289f38f5438
fpenonori <fpenonori@gmail.com>
2024-10-26 21:03:33 -0300

    Fix: Hardcoded connection string

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 //LO HARDCODEO XQ NO LO ESTA TOMANDO DE  .ENV
-const sequelize = new Sequelize("postgresql://db-user:NEdcKVk7NYQB7DMb-lPLMg@linkandlearn-15961.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full", {
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
     logging: false,
 });
 // const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
