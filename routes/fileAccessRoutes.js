716605dcfa9c8232f6f3f3cefdd4054d0283a4c5
fpenonori <fpenonori@gmail.com>
2024-11-05 04:31:02 -0300

    Protected routes

--- routes/fileAccessRoutes.js
+++ routes/fileAccessRoutes.js
@@ -9,18 +9,18 @@ const {
 } = require('../controllers/fileAccessController');
 
 const app = express();
-//const authorizeRoles = require('../middleware/authMiddleware');
+const authorizeRoles = require('../middleware/authMiddleware');
 
 app.use(express.json());
 
 const router = express.Router();
 
-router.post('/grant/:id', grantAccess);
-router.delete('/revoke/:fileId', revokeAccess);
-router.get('/teacher/:teacher_id', getFilesForTeacher);
-router.get('/student/:student_id', getFilesForStudent);
-router.get('/all-students-granted/:file_id', getGrantedStudentsForFile);
-router.get('/eligible-students/:fileId', getEligibleStudents);
+router.post('/grant/:id', authorizeRoles('TEACHER'), grantAccess);
+router.delete('/revoke/:fileId', authorizeRoles('TEACHER'), revokeAccess);
+router.get('/teacher/:teacher_id', authorizeRoles('TEACHER'), getFilesForTeacher);
+router.get('/student/:student_id', authorizeRoles('STUDENT'),getFilesForStudent);
+router.get('/all-students-granted/:file_id', authorizeRoles('TEACHER'), getGrantedStudentsForFile);
+router.get('/eligible-students/:fileId', authorizeRoles('TEACHER'), getEligibleStudents);
 
 
 module.exports = router;
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -7,15 +7,15 @@ const {
 } = require('../controllers/fileController');
 const app = express();
 const upload = require('../config/multerConfig');
-//const authorizeRoles = require('../middleware/authMiddleware');
+const authorizeRoles = require('../middleware/authMiddleware');
 
 app.use(express.json());
 
 const router = express.Router();
 
-router.post('/upload-single', upload.single('file'), uploadSingleFile);
-router.post('/:id', getSingleFile);
-router.put('/update-single/:id', renameSingleFile);
-router.delete('/delete-single/:id', deleteSingleFile);
+router.post('/upload-single', upload.single('file'), authorizeRoles('TEACHER'), uploadSingleFile);
+router.post('/:id', authorizeRoles('TEACHER', 'STUDENT'), getSingleFile);
+router.put('/update-single/:id', authorizeRoles('TEACHER'),renameSingleFile);
+router.delete('/delete-single/:id', authorizeRoles('TEACHER'), deleteSingleFile);
 
 module.exports = router;
