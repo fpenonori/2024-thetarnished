38274fc113d0908f059899613001e92332462c9e
fpenonori <fpenonori@gmail.com>
2025-10-13 18:28:22 -0300

    Fixed reasonable expiry timeout and disabled debugging routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -15,10 +15,10 @@ const router = express.Router();
 router.post('/activate-teacher/:id', authorizeRoles('ADMIN'), activateTeacher)
 router.post('/disable-teacher/:id', authorizeRoles('ADMIN'), disableTeacher)
 router.get('/inactive-teachers', authorizeRoles('ADMIN'), getInactiveTeachers)
-router.get('/test-cron', authorizeRoles('ADMIN'), testCron)
-router.post('/populate-db', authorizeRoles('ADMIN'), populateDB)
-router.post('/seed-schedule', authorizeRoles('ADMIN'), seedTeacherSchedule)
-router.delete('/wipe-db', authorizeRoles('ADMIN'), wipeAllModeledTables)
+//router.get('/test-cron', authorizeRoles('ADMIN'), testCron)
+//router.post('/populate-db', authorizeRoles('ADMIN'), populateDB)
+//router.post('/seed-schedule', authorizeRoles('ADMIN'), seedTeacherSchedule)
+//router.delete('/wipe-db', authorizeRoles('ADMIN'), wipeAllModeledTables)
 
 
 module.exports = router;
